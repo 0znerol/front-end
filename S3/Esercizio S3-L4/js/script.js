33f059ca5a57f495c6a5747393856b72d2e10b08
0znerol <lorenzo.pezzica98@gmail.com>
2023-11-10 17:42:19 +0100

    modified:   S3/Esercizio S3-L4/css/style.css 	modified:   S3/Esercizio S3-L4/index.html 	modified:   S3/Esercizio S3-L4/js/script.js

--- S3/Esercizio S3-L4/js/script.js
+++ S3/Esercizio S3-L4/js/script.js
@@ -1,29 +1,68 @@
 let estractButton = document.getElementById("estractButton");
-
+let generateTabButton = document.getElementById("genPTab");
 window.onload = function () {
+  generateExtractionTab();
+};
+function generateExtractionTab() {
   for (i = 1; i < 77; i++) {
     let elementoTabella = `
-                            
-                                <h3>${i}</h3>
-                            
-    `;
+                                
+                                    <h3>${i}</h3>
+                                
+        `;
     document.getElementById("tabNumeriEstratti").innerHTML += elementoTabella;
   }
-};
+}
 
-console.dir(estractButton);
+generateTabButton.addEventListener("click", (event) => {
+  generatePlayerTab();
+});
+
+function generatePlayerTab() {
+  let playerTabArr = [];
+
+  for (i = 1; i < 25; i++) {
+    let playerTabNum = Math.round(Math.random() * 77);
+    if (!playerTabArr.includes(playerTabNum)) {
+      playerTabArr.push(playerTabNum);
+      let elementoTabella = `
+                                
+            <h3>${playerTabNum}</h3>
+        
+    `;
+      document.getElementById("playerTab").innerHTML += elementoTabella;
+    } else {
+      i--;
+    }
+  }
+  document.getElementById(
+    "playerTab"
+  ).innerHTML += `<div class="spacerDiv"></div>`;
+}
+
+let rngArr = [];
 let estractNumber = (button) => {
   button.addEventListener("click", (evt) => {
-    let rng = Math.round(Math.random() * 77);
-    numCheck(rng);
+    generteNCheck();
   });
 };
 estractNumber(estractButton);
 
-function numCheck(genertedNum) {
+function generteNCheck() {
+  let rng = Math.round(Math.random() * 77);
+
+  if (!rngArr.includes(rng)) {
+    rngArr.push(rng);
+    changeBG(rng);
+  } else {
+    generteNCheck();
+  }
+}
+
+function changeBG(genertedNum) {
   let tabNum = document.querySelectorAll("h3");
   for (const key in tabNum) {
-    console.log(tabNum[key].innerHTML);
+    // console.log(tabNum[key].innerHTML);
 
     if (tabNum[key].innerHTML === String(genertedNum)) {
       tabNum[key].classList.add("active");
