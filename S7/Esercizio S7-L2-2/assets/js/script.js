9aa4cf399f6f445a2d4924a5baad43770d726851
0znerol <lorenzo.pezzica98@gmail.com>
2023-12-07 00:57:39 +0100

    new file:   S7/Esercizio S7-L2-2/assets/css/style.css

--- S7/Esercizio S7-L2-2/assets/js/script.js
+++ S7/Esercizio S7-L2-2/assets/js/script.js
@@ -3,23 +3,33 @@ let inputButton = document.querySelector("button");
 let delButton = document.querySelectorAll("button")[1];
 let label = document.querySelector("label");
 let todoUl = document.querySelector("ul");
+let buttonUl = document.querySelectorAll("ul")[1];
 let li = document.querySelectorAll("li");
 let cc = localStorage.getItem("counter");
 let todoArr = [];
+let btArr = [];
+let strikeCc = localStorage.getItem("strikeCc");
 let i = 0;
+console.log(cc);
 addTodo();
 inputButton.addEventListener("click", () => {
-  cc++;
-  localStorage.setItem("counter", cc);
-  let usrInput = input.value;
-  todoArr.push[usrInput];
-  localStorage.setItem(`${cc}`, usrInput);
-  // console.dir(localStorage.getItem(`${cc}`));
-  // console.log(cc);
-  // console.log(todoArr);
-  // console.log(localStorage.getItem("counter"));
-  input.value = "";
-  addTodo();
+  if (input.value != "") {
+    cc++;
+    localStorage.setItem("counter", cc);
+    let usrInput = input.value;
+    todoArr.push[usrInput];
+    localStorage.setItem(`${cc}`, usrInput);
+    // console.dir(localStorage.getItem(`${cc}`));
+    // console.log(cc);
+    // console.log(todoArr);
+    // console.log(localStorage.getItem("counter"));
+    input.value = "";
+    // buttonUl.innerHTML += `<li><button><i class="bi bi-trash"></i></button></li>`;
+
+    addTodo();
+  } else {
+    alert("sticazi");
+  }
 });
 delButton.addEventListener("click", () => {
   localStorage.removeItem("counter");
@@ -28,9 +38,15 @@ delButton.addEventListener("click", () => {
 function addTodo() {
   for (i = i; i <= cc; i++) {
     todoArr.push(localStorage.getItem(`${i}`));
+    btArr.push(localStorage.getItem(`${i}`));
+
     console.log(todoArr);
-    todoUl.innerHTML += `<li>${todoArr[i - 1]}</li>`;
-    document.querySelector("li").style.display = "none";
+    if (todoArr[i - 1] != undefined) {
+      todoUl.innerHTML += `<li class="d-flex justify-content-between border rounded p-1">
+      ${todoArr[i - 1]}
+    <button class="btn btn-outline-danger"><i class="bi bi-trash"></i></button> </li>`;
+    } else {
+    }
     if (i === 0) {
       todoArr.shift();
     }
@@ -39,7 +55,10 @@ function addTodo() {
 todoUl.addEventListener("click", (event) => {
   console.log(event.target.nodeName);
   console.dir(event.target);
+
   if (event.target.nodeName === "LI") {
+    strikeCc++;
+    localStorage.setItem("counter", cc);
     event.target.style.textDecoration = "line-through";
   }
 });
