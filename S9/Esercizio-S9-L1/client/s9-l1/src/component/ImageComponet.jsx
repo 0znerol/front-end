485283db177e8d4369bff67c36914933f33358e1
0znerol <lorenzo.pezzica98@gmail.com>
2024-01-08 17:35:21 +0100

    modified:   S9/Esercizio-S9-L1/client/s9-l1/src/component/ImageComponet.jsx

--- S9/Esercizio-S9-L1/client/s9-l1/src/component/ImageComponet.jsx
+++ S9/Esercizio-S9-L1/client/s9-l1/src/component/ImageComponet.jsx
@@ -2,9 +2,6 @@ import { Component } from "react";
 import React from "react";
 
 export default class ImageComponent extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return <img src={this.props.src} alt="Image" className="rounded shadow" />;
   }
